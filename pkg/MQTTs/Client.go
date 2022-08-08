18ba93599aadb4ca9c863d54f2955f0b4e356697
adamhoof <adam.homfna1@gmail.com>
2022-08-08 19:58:17 +0200

    rewrite MQTTCLient to implement elcipse library Client interface -> not having to write a wrapper

--- pkg/MQTTs/Client.go
+++ pkg/MQTTs/Client.go
@@ -1,77 +1,77 @@
 package mqtts
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"fmt"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
-	"io/ioutil"
 	"log"
 )
 
-type Client struct {
-	clientOptions mqtt.ClientOptions
-	client        mqtt.Client
+type MQTTClient struct {
 }
 
-const (
-	brokerName = "tls://proteccmqtt.medunka.cz:8883"
-	clientName = "RPICommandHandler"
-)
-
-func (client *Client) SetupClientOptions() {
-	client.clientOptions.AddBroker(brokerName)
-	client.clientOptions.SetClientID(clientName)
-	client.clientOptions.SetTLSConfig(client.GenerateTlsConfig())
-	client.clientOptions.SetAutoReconnect(true)
-	client.clientOptions.SetConnectRetry(true)
-	client.clientOptions.SetCleanSession(false)
-	client.clientOptions.SetOrderMatters(false)
+func (client *MQTTClient) SetOptions(options mqtt.ClientOptions) {
+	client.SetOptions(options)
 }
 
-func (client *Client) GenerateTlsConfig() *tls.Config {
-	certpool := x509.NewCertPool()
-	ca, err := ioutil.ReadFile("Auth/ca/ca.crt")
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-	certpool.AppendCertsFromPEM(ca)
+func (client *MQTTClient) Connect() mqtt.Token {
+	token := client.Connect()
 
-	certificateKeyPair, certReadingErr := tls.LoadX509KeyPair("Auth/client/client.crt", "Auth/client/client.key")
-
-	if certReadingErr != nil {
-		panic(certReadingErr)
-	}
-
-	return &tls.Config{
-		RootCAs:            certpool,
-		InsecureSkipVerify: true,
-		ClientAuth:         tls.RequireAndVerifyClientCert,
-		Certificates:       []tls.Certificate{certificateKeyPair},
-	}
-}
-
-func (client *Client) CreateClient() {
-	client.client = mqtt.NewClient(&client.clientOptions)
-}
-
-func (client *Client) SetSubscription(messageProcessor mqtt.MessageHandler, topic string) {
-
-	if token := (client.client).Subscribe(topic, 0, messageProcessor); token.Wait() && token.Error() != nil {
-		log.Fatalf("failed to create subscription: %v", token.Error())
-	}
-}
-
-func (client *Client) ConnectClient() {
-	if token := (client.client).Connect(); token.Wait() && token.Error() != nil {
+	if token.Wait() && token.Error() != nil {
 		log.Fatalf("failed to create connection: %v", token.Error())
 	}
-	fmt.Println("mqtt client connected")
+	fmt.Println("mqtt handler connected")
+	return token
 }
 
-func (client *Client) PublishText(topic string, payload string) {
+func (client *MQTTClient) IsConnected() bool {
+	return client.IsConnected()
+}
 
-	if token := (client.client).Publish(topic, 0, true, payload); token.Wait() && token.Error() != nil {
+func (client *MQTTClient) IsConnectionOpen() bool {
+	return client.IsConnectionOpen()
+}
+
+func (client *MQTTClient) Disconnect(quiesce uint) {
+	client.Disconnect(quiesce)
+}
+
+func (client *MQTTClient) Publish(topic string, qos byte, retained bool, payload interface{}) mqtt.Token {
+	token := client.Publish(topic, qos, retained, payload)
+
+	if token.Wait() && token.Error() != nil {
 		log.Fatalf("failed to send upd: %v", token.Error())
 	}
+	return token
+}
+
+func (client *MQTTClient) PublishText(topic string, payload string) mqtt.Token {
+	token := client.Publish(topic, 0, true, payload)
+
+	if token.Wait() && token.Error() != nil {
+		log.Fatalf("failed to send upd: %v", token.Error())
+	}
+	return token
+}
+
+func (client *MQTTClient) Subscribe(topic string, qos byte, callback mqtt.MessageHandler) mqtt.Token {
+	token := client.Subscribe(topic, 0, callback)
+	if token.Wait() && token.Error() != nil {
+		log.Fatalf("failed to create subscription: %v", token.Error())
+	}
+	return token
+}
+
+func (client *MQTTClient) SubscribeMultiple(filters map[string]byte, callback mqtt.MessageHandler) mqtt.Token {
+	return client.SubscribeMultiple(filters, callback)
+}
+
+func (client *MQTTClient) Unsubscribe(topics ...string) mqtt.Token {
+	return client.Unsubscribe(topics...)
+}
+
+func (client *MQTTClient) AddRoute(topic string, callback mqtt.MessageHandler) {
+	client.AddRoute(topic, callback)
+}
+func (client *MQTTClient) OptionsReader() mqtt.ClientOptionsReader {
+	return client.OptionsReader()
 }
