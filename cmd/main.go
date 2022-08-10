5654a087bc08688adf9b2b64ece39ac823d727d5
adamhoof <adam.homfna1@gmail.com>
2022-08-10 17:25:40 +0200

    implement logic from changed files

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,11 @@ import (
 	connectable "RPICommandHandler/pkg/ConnectableDevices"
 	database "RPICommandHandler/pkg/Database"
 	env "RPICommandHandler/pkg/Env"
-	mqtt2 "RPICommandHandler/pkg/MQTT"
 	telegram "RPICommandHandler/pkg/Telegram"
 	"fmt"
-	mqtt "github.com/eclipse/paho.mqtt.golang"
+	pahomqtt "github.com/eclipse/paho.mqtt.golang"
 	_ "github.com/lib/pq"
+	tb "gopkg.in/telebot.v3"
 	"os"
 	"sync"
 )
@@ -16,31 +16,26 @@ import (
 func main() {
 	env.SetEnv()
 
-	mqttClient := mqtt2.MQTTClient{}
+	options := pahomqtt.ClientOptions{}
+	options.AddBroker(os.Getenv("mqttServer"))
+	options.SetClientID(os.Getenv("mqttClientName"))
+	options.SetAutoReconnect(true)
+	options.SetConnectRetry(true)
+	options.SetCleanSession(false)
+	options.SetOrderMatters(false)
+	// use options.SetTLSConfig if you want to establish secure connection (not required on localhost, recommended when connecting to remote server)
+	mqttClient := pahomqtt.NewClient(&options)
+	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	fmt.Println("MQTT client connection established?: ", mqttClient.IsConnected())
+
 	postgresHandler := database.PostgresHandler{}
-	me := telegram.User{Id: os.Getenv("telegramBotOwner")}
-	botHandler := telegram.BotHandler{Owner: me}
 
 	var routineSyncer sync.WaitGroup
 	routineSyncer.Add(1)
-	go func(routineSyncer *sync.WaitGroup, client *mqtt2.MQTTClient) {
-		defer routineSyncer.Done()
-		options := mqtt.ClientOptions{}
-		options.AddBroker(os.Getenv("mqttServer"))
-		options.SetClientID(os.Getenv("mqttClientName"))
-		options.SetAutoReconnect(true)
-		options.SetConnectRetry(true)
-		options.SetCleanSession(false)
-		options.SetOrderMatters(false)
-		// use options.SetTLSConfig if you are running MQTT broker on remote server instead of localhost
-		client.SetOptions(options)
-		client.Connect()
-	}(&routineSyncer, &mqttClient)
-
-	routineSyncer.Add(1)
 	go func(syncer *sync.WaitGroup, handler database.DatabaseHandler) {
 		defer syncer.Done()
-
 		dbConnectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s",
 			os.Getenv("dbHost"),
 			os.Getenv("dbPort"),
@@ -54,14 +49,17 @@ func main() {
 	}(&routineSyncer, &postgresHandler)
 	routineSyncer.Wait()
 
-	keyboardStorage := telegram.KeyboardStorage{}
-	telegram.CreateAllToysKeyboardUI(&botHandler, &keyboardStorage)
-	telegram.CreateOfficeToysKeyboardUI(&botHandler, &keyboardStorage)
-	telegram.CreateBedroomToysKeyboardUI(&botHandler, &keyboardStorage)
+	me := telegram.User{Id: os.Getenv("telegramBotOwner")}
+	botHandler := telegram.BotHandler{Owner: me}
+	botHandler.CreateBot(os.Getenv("telegramBotToken"))
+
+	keyboards := make(map[string]*tb.ReplyMarkup)
+	telegram.CreateAllToysKeyboardUI(&botHandler, keyboards)
+	telegram.CreateOfficeToysKeyboardUI(&botHandler, keyboards)
+	telegram.CreateBedroomToysKeyboardUI(&botHandler, keyboards)
 
 	toys := make(map[string]*connectable.Toy)
 	postgresHandler.PullToyData(toys)
 
-	botHandler.CreateBot(os.Getenv("telegramBotToken"))
 	botHandler.StartBot()
 }
